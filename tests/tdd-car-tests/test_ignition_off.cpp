1ec2ebaafdbba94052ae038ee99e852368c2990c
Ammaniel <aaraia@gmail.com>
2021-03-23 18:52:37 +0000

    Ignition Off: Failing Test

---
+++ tests/tdd-car-tests/test_ignition_off.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+
+//  car
+#include "../../include/car.h"
+
+TEST(CarTests, IgnitionOff) {
+    using CarNS::Car;
+    Car car;
+
+    //  by default when a new car is made, its ignition should be off
+    ASSERT_TRUE(car.IsIgnitionOn() == true);
+}
