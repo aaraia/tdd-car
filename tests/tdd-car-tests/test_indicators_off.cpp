9e4289160b49e64286f73561e601935ef0c6bffa
Ammaniel <aaraia@gmail.com>
2021-03-24 16:01:07 +0000

    Indicators Off: Failing Test

--- tests/tdd-car-tests/test_indicators_off.cpp
+++ tests/tdd-car-tests/test_indicators_off.cpp
@@ -6,4 +6,28 @@
 TEST(CarTests, IndicatorsOff) {
     using CarNS::Car;
     Car car;
+
+    //  true the car on
+    car.ignition(true);
+    ASSERT_TRUE(car.isIgnitionOn());
+
+    //  indicators should be off, as car has just been switched on
+    bool left = true;
+    ASSERT_TRUE(car.isInidcatorOn(left) == false);
+    ASSERT_TRUE(car.isInidcatorOn(!left) == false);
+
+    //  switch on indicators
+    car.indicator(left, true);
+    ASSERT_TRUE(car.isInidcatorOn(left));
+
+    car.indicator(!left, true);
+    ASSERT_TRUE(car.isInidcatorOn(!left));
+
+    //  switch off indicators
+    car.indicator(left, false);
+    ASSERT_TRUE(car.isInidcatorOn(left));
+
+    car.indicator(!left, false);
+    ASSERT_TRUE(car.isInidcatorOn(!left));
+
 }
