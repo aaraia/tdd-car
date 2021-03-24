d17b0f7ca37fa29540d237a92d84d15b1e076c86
Ammaniel <aaraia@gmail.com>
2021-03-24 16:07:23 +0000

    Indicators Off: Min Code To Pass Test

--- include/car.h
+++ include/car.h
@@ -4,13 +4,15 @@ namespace CarNS {
 
     class Car {
         bool m_on = false;
+        bool m_leftIndicator = false;
+        bool m_rightIndicator = false;
 
     public:
         bool isIgnitionOn() const { return m_on; }
         void ignition(const bool turnOn) { m_on = turnOn; }
 
-        bool isInidcatorOn(const bool left) const { return false; }
-        void indicator(const bool left, const bool state) {  }
+        bool isInidcatorOn(const bool left) const { return left ? m_leftIndicator : m_rightIndicator; }
+        void indicator(const bool left, const bool state) { left ? (m_leftIndicator = state) : (m_rightIndicator = state); }
     };
     
 }
--- tests/tdd-car-tests/test_indicators_off.cpp
+++ tests/tdd-car-tests/test_indicators_off.cpp
@@ -13,21 +13,22 @@ TEST(CarTests, IndicatorsOff) {
 
     //  indicators should be off, as car has just been switched on
     bool left = true;
+    bool right = false;
     ASSERT_TRUE(car.isInidcatorOn(left) == false);
-    ASSERT_TRUE(car.isInidcatorOn(!left) == false);
+    ASSERT_TRUE(car.isInidcatorOn(right) == false);
 
     //  switch on indicators
     car.indicator(left, true);
     ASSERT_TRUE(car.isInidcatorOn(left));
 
-    car.indicator(!left, true);
-    ASSERT_TRUE(car.isInidcatorOn(!left));
+    car.indicator(right, true);
+    ASSERT_TRUE(car.isInidcatorOn(right));
 
     //  switch off indicators
     car.indicator(left, false);
-    ASSERT_TRUE(car.isInidcatorOn(left));
+    ASSERT_TRUE(car.isInidcatorOn(left) == false);
 
-    car.indicator(!left, false);
-    ASSERT_TRUE(car.isInidcatorOn(!left));
+    car.indicator(right, false);
+    ASSERT_TRUE(car.isInidcatorOn(right) == false);
 
 }
