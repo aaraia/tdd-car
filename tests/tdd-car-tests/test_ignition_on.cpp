5f1b1631be1690ac3d9272903547c401b99d310c
Ammaniel <aaraia@gmail.com>
2021-03-24 15:18:03 +0000

    Ignition On: Failing Test

---
+++ tests/tdd-car-tests/test_ignition_on.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+
+//  car
+#include "../../include/car.h"
+
+TEST(CarTests, IgnitionOn) {
+    using CarNS::Car;
+
+    //  create a car
+    Car car;
+
+    //  turn on the ignition
+    car.ignition(true);
+
+    ASSERT_TRUE(car.IsIgnitionOn());
+}
